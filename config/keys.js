4c73dec2dc0c542147acf0e00d5fd3f9c6405082
Josh Taningco <josh.taningco@gmail.com>
2021-10-07 01:46:23 +0800

    added security

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: "mongodb+srv://josh:DFK19Dbww6nd5eVT@murchwaitlist.mopvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    mongoURI: process.env.MONGO_URI || "mongodb+srv://josh:DFK19Dbww6nd5eVT@murchwaitlist.mopvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
